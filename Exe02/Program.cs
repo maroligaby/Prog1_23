2bdb9c062461ef46699eb40a0b4eb476e28e6b2d
Gabrielly Maroli <403302@unoesc.lan>
2023-03-06 22:09:17 -0300

    exercise 2

---
+++ Exe02/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("olá, seja bem vindo!");
+Console.WriteLine("Qual é o seu nome?");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Olá, {name} prazer conhecê-lo.");
+
+//Declarar sem inicializar / primitivo
+string message1;
+//declarar inicializado / abstrato
+String message2 = "Seu nome completo";
+//declarar string vazia
+string message3 = String.Empty;
+string message4 = "";
+//declaração com tipo implicito
+var message5 = "mensagem aleatória";
+
+message1 = "Rogerio Ceni;pelé;the rock;Roberto Carlos";
+var palavras = message1.Split(';');
+foreach(var word in palavras)
+{
+    //variavel de escopo local do laço de repetição foreach
+    int a = 10;
+    Console.WriteLine(word);
+}
