06abbbf1657774fa11c5f82f83b7250a2fe31f22
Gabrielly Maroli <403302@unoesc.lan>
2023-03-13 21:29:10 -0300

    teste_13_03

---
+++ Exe03/Program.cs
@@ -0,0 +1,18 @@
+﻿static void FormatString(
+    string stringToFormat)
+{
+    stringToFormat.Replace("Mundo", "Marte");
+}
+
+static string FormatReturningString(string stringToFormat)
+{
+    return stringToFormat.Replace("Mundo", "Marte");
+}
+
+var mensagem = "Olá Mundo!";
+//Invocando o método sem retormo
+FormatString(mensagem);
+Console.WriteLine(mensagem);
+
+var outraMensagem = "Bom dia Mundo!";
+Console.WriteLine( FormatReturningString(outraMensagem) );
---
+++ Exe04/Program.cs
@@ -0,0 +1,21 @@
+﻿var myFloat = 10f;
+float meuFloat = 11;
+
+//conversão de tipo numérico 
+int myInteger = 100;
+meuFloat = (float)myInteger;
+decimal myDecimal = Convert.ToDecimal(myInteger);
+
+var myDouble = 10d;
+myDecimal =10m;
+
+string meuNumeroTexto = Convert.ToString(myInteger);
+int qtdchars = meuNumeroTexto.Length;
+for(int i = 0; i<qtdchars; i++)
+//parametros do laço de repetição:
+//i<qtdchars = flag
+//i=0 = contador
+//i++ = acumulador
+{
+    Console.WriteLine(meuNumeroTexto[i]);
+}
