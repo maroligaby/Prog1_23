d7b145995ef215ddebeb5bc02e84dc06dd378dd3
maroligaby <gabymaroli@hotmail.com>
2023-03-17 09:57:39 -0300

    exe_cheque

---
+++ ExeCheque/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("digite o número que deseja escrever por extenso");
+var numeroRecebido = Console.ReadLine();
+//Console.WriteLine (numeroRecebido);
+string numeroString = Convert.ToString(numeroRecebido);
+int qtdchars = numeroString.Length;
+Console.WriteLine (qtdchars);
+
+if (qtdchars) == 1
+    
+    for(int i = 0; i<qtdchars; i++)
+    {
+        Console.WriteLine(numeroString[i]); 
+    }
+
