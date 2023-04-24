cdd374946f00d93fd6d35a97143635aa571bc7be
maroligaby <gabymaroli@hotmail.com>
2023-04-24 11:59:53 -0300

    teste

---
+++ Atividade_01/Classes/Operario.cs
@@ -0,0 +1,8 @@
+namespace Atividade_01.Classes
+{
+    public class Operario :Pessoa
+    {
+        public string? Funcao;
+        public bool dirige;
+    }
+}
---
+++ Atividade_01/Classes/Pessoa.cs
@@ -0,0 +1,10 @@
+namespace Atividade_01.Classes
+{
+    public class Pessoa
+    {
+        public string? Name;
+        public DateTime Nascimento;
+        public int CodigoFuncionario;
+        public DateTime contratacao;
+    }
+}
